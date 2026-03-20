c6e9bedbea18372c9abfdf2c9413fecac220a23f
bedirhanteke <bedirhanteke00@gmail.com>
2026-03-20 21:20:55 +0300

    Tertemiz başlangıç: Gereksiz dosyalar temizlendi

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // NotHesaplama sınıfındaki hesaplama metodunu çağırıyoruz
+        NotHesaplama.hesaplama(); 
+    }
+}
---
+++ src/NotHesaplama.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class NotHesaplama {
+    public static void hesaplama() {
+        // Burada adı 'scan' koydun:
+        Scanner scan = new Scanner(System.in); 
+        
+        System.out.println("selam");
+        System.out.println("Vize notunuzu giriniz:");
+
+        // HATA BURADAYDI: 'scanner' yerine 'scan' yazmalısın
+        int vize_notu = scan.nextInt(); 
+        
+        System.out.println("Final notunuzu giriniz:");
+        
+        // HATA BURADAYDI: Yine 'scan' olmalı
+        int final_notu = scan.nextInt();
+
+        double ortalama = (vize_notu * 0.4) + (final_notu * 0.6);
+        System.out.println("Ortalamanız: " + ortalama);
+    }
+}
