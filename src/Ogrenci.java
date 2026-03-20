3ffadf2795c018407b8625528f763fbe1cb31cfb
Bedirhan Teke <bedirhan.teke00@gmail.com>
2026-03-20 21:44:00 +0300

    "palindrome sayı örneği"

---
+++ src/Ogrenci.java
@@ -0,0 +1,10 @@
+public class Ogrenci {
+    public String ad;
+    public int numara;
+
+    // Bilgileri ekrana yazdıran bir metot
+    public void bilgileriGoster() {
+        System.out.println("Öğrenci Adı: " + ad);
+        System.out.println("Öğrenci Numarası: " + numara);
+    }
+}
